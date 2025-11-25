9e7b6594969ac96be5855ab6371355a301de25a4
Martín (Netux) Rodriguez <send.email@netux.site>
2025-11-25 10:13:25 -0300

    Fix typos; reword, and format sections
    Major changes include:
    - Remove note about why the game does "current level + 1". It was distracting from the rest of the tutorial.
    - Explain more of what the IL Cursor can do
    - Explain more about how we found the `ldc.i4 5` instruction we want to replace.

--- HasteEndlessRewardLevelRateMod/RewardEndlessLevelRateMod.cs
+++ HasteEndlessRewardLevelRateMod/RewardEndlessLevelRateMod.cs
@@ -9,14 +9,14 @@ namespace HasteEndlessRewardLevelRateMod;
 [LandfallPlugin]
 public class RewardEndlessLevelRateMod
 {
-    // This class defines a setting that will show up in the in-game Setting menu.
-    // Landfall has made this easy for us. We can simply inherit from Zorro.Settings.IExposedSetting
-    // and implement any necessary methods.
-    // We also inherit from IntSetting, since this is a setting that stores an int (whole number).
+    // This class defines a setting that will show up on the in-game Setting menu.
+    // Landfall has made this easy for us! We can simply inherit from Zorro.Settings.IExposedSetting and
+    // implement any necessary methods.
+    // We also inherit from IntSetting, since we are creating a setting that stores an integer (a whole number).
     //
-    // This [HasteSetting] thing is just a little annotation Haste gives us to register our setting.
+    // This "[HasteSetting]" thing is an annotation Landfall gives us to quickly register setting.
     // The alternative would be to call `GameHandler.Instance.SettingsHandler.AddSetting(new OurSetting());`
-    // on a hook to `GameHandler.Awake`. But this way is much simpler and saves us a hook.
+    // on a hook to `GameHandler.Awake` or something similar to that.
     [HasteSetting]
     public class EndlessRewardEveryLevelSetting : IntSetting, IExposedSetting
     {
@@ -31,44 +31,49 @@ public class RewardEndlessLevelRateMod
     static RewardEndlessLevelRateMod()
     {
         /* 
-         * Setup an IL hook to modify the IL code of RunHandler.TransitionOnLevelComplete. As the name
-         * implies, this function does the transition when a level completes (both when in a shard and
-         * in Endless).
+         * Setup an IL hook to modify the IL code of RunHandler.TransitionOnLevelComplete.
+         * As the name implies, this function manages the transition when a level completes (both when in
+         * a shard and in Endless).
          * 
-         * The logic we'll be targetting here is the following statement:
+         * The logic we'll be overwriting here is in the following statement:
          * 
-         * if (
-         *   RunHandler.RunData.runConfig.isEndless &&
-         *   (RunHandler.RunData.currentLevel == 0 || (RunHandler.RunData.currentLevel + 1) % 5 == 0)
-         * ) {
-         *   // ... transition to Endless Award scene ...
-         * }
+         *   if (
+         *     RunHandler.RunData.runConfig.isEndless &&
+         *     (RunHandler.RunData.currentLevel == 0 || (RunHandler.RunData.currentLevel + 1) % 5 == 0)
+         *   ) {
+         *     // ... transition to Endless Award scene ...
+         *   }
          * 
-         * More specifically, the condition `(RunHandler.RunDate.currentLevel + 1) % 5 == 0`, which
-         * passes only when the current level* is divisible by 5.
+         * More specifically, the condition `(RunHandler.RunDate.currentLevel + 1) % 5 == 0`, which returns
+         * true only when the current level number is divisible by 5.
          * 
-         * *: the game does currentLevel + 1 because levels numbers internally start at 0.
-         * 
-         * Note that the 5 is a constant in the code. We cannot change it by simply changing a variable
-         * something like that.
+         * Note that the 5 is a constant in the code. It cannot be changed by overwriting a variable, for example.
          * Instead, we have to modify the IL code directly:
          */
         IL.RunHandler.TransitionOnLevelCompleted += (il) =>
         {
-            // Create a cursor that will be used to edit the IL code programatically.
-            // This is similar to the text cursor for when you're editing text!
+            // Create a cursor. This will be used to edit the IL code programatically.
+            // You can think of this ILCursor as the cursor that shows up when you're editing text:
+            // - The cursor always points to a specific character (IL instruction)
+            // - The cursor can be used to edit that character (get, update, add, remove, plus some other helpers)
+            // To start, the IL cursor will be set on the very first instruction of the method.
             var cursor = new ILCursor(il);
 
-            // Put the cursor to the instruction that loads the number 5 into memory
-            // Note that the exact instruction here was found using dnSpyEx (ILSpy also
-            // works) in IL with C# mode.
+            // Put the cursor on the instruction that loads the number 5 into memory
+            // The exact instruction here was found using a tool like dnSpyEx or ILSpy. You can try finding this
+            // instruction by using one of those tools in a mode that shows both the IL code and the C# code
+            // simultaneously.
+            //
+            // Loading up the game's Assembly-CSharp.dll, find the `RunHandler.TransitionOnLevelCompleted` method,
+            // and look for if-statement described above.
+            // You will also see that this is the only `ldc.i4 5` instruction in the entire method, so it is quite
+            // safe to just find the first instance of that instruction and assume it is the one we want to change.
             cursor.TryGotoNext((i) => i.MatchLdcI4(5)); 
 
             // Remove the instruction
             cursor.Remove();
 
-            // And in its place, put the following code.
-            // This code...
+            // And in its place, put the following code:
             cursor.EmitDelegate(() =>
             {
                 // Gets a reference to the setting we created.
